b8e4bc386d7c175f27b677a6f598411869f4dc89
SantiiCapee <santiicapee@gmail.com>
2026-04-20 00:44:20 -0300

    cambios en page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,22 @@
 export default function Home() {
-  return <h1> Bienvenido a Rocket Pizza</h1>;
+  return (
+    <main className="h-screen flex items-center justify-center bg-black">
+      <div className="relative w-full h-full">
+        <img
+          src="/rocketpizza.png"
+          alt="Pizza"
+          className="w-full h-full object-cover opacity-60"
+        />
+
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+          <h1 className="text-white text-5xl font-bold mb-4">
+            Rocket Pizza 🚀
+          </h1>
+          <p className="text-white text-lg">
+            Sabores clásicos como la pizza de mamá
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
